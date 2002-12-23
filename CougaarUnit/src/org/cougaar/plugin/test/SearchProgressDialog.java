eb0a442ed88f5f3208d7266933ca13804b82b801
dave <dave>
2002-12-23 22:30:05 +0000

    *** empty log message ***

---
+++ CougaarUnit/src/org/cougaar/plugin/test/SearchProgressDialog.java
@@ -0,0 +1,93 @@
+package org.cougaar.plugin.test;
+
+import java.awt.*;
+import javax.swing.*;
+import com.borland.jbcl.layout.*;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+public class SearchProgressDialog extends JDialog {
+    private JPanel panel1 = new JPanel();
+    private BorderLayout borderLayout1 = new BorderLayout();
+    private JPanel jPanel1 = new JPanel();
+    private VerticalFlowLayout verticalFlowLayout1 = new VerticalFlowLayout();
+    private JLabel jLabelNote = new JLabel();
+    private JLabel jLabelHeading = new JLabel();
+    private JProgressBar jProgressBar1 = new JProgressBar();
+
+    public SearchProgressDialog(Frame frame, String title, boolean modal) {
+        super(frame, title, modal);
+        try {
+            jbInit();
+            pack();
+            init2();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    private void init2() {
+        setSize(300,100);
+        center();
+        show();
+    }
+
+    public SearchProgressDialog(String heading, String note, int min, int max) {
+        this(null, "", false);
+        this.jLabelHeading.setText(heading);
+        this.jLabelNote.setText(note);
+        if (max <= 0)  {
+            jProgressBar1.setIndeterminate(true);
+        }
+        else {
+            jProgressBar1.setMinimum(min);
+            jProgressBar1.setMaximum(max);
+        }
+    }
+
+    private void jbInit() throws Exception {
+        panel1.setLayout(borderLayout1);
+        jPanel1.setLayout(verticalFlowLayout1);
+        jLabelNote.setToolTipText("");
+        jLabelNote.setText("Note");
+        jLabelHeading.setToolTipText("");
+        jLabelHeading.setHorizontalAlignment(SwingConstants.CENTER);
+        jLabelHeading.setText("Seach Progress Dialog");
+        getContentPane().add(panel1);
+        panel1.add(jPanel1, BorderLayout.CENTER);
+        jPanel1.add(jLabelNote, null);
+        panel1.add(jLabelHeading, BorderLayout.NORTH);
+        jPanel1.add(jProgressBar1, null);
+    }
+
+    public void setNote(String text) {
+        jLabelNote.setText(text);
+    }
+
+    public void setHeading(String text) {
+        jLabelHeading.setText(text);
+    }
+
+    public void setProgressValue(int value) {
+        jProgressBar1.setValue(value);
+    }
+
+    public void close() {
+        this.dispose();
+    }
+
+    private void center () {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension windowSize = this.getSize();
+        this.setLocation((screenSize.width - windowSize.width)/2, (screenSize.height - windowSize.height)/2);
+    }
+}
--- CougaarUnit/src/org/cougaar/plugin/test/UI.java
+++ CougaarUnit/src/org/cougaar/plugin/test/UI.java
@@ -9,6 +9,10 @@ import java.io.File;
 import java.util.jar.JarFile;
 import java.util.Enumeration;
 import java.util.jar.JarEntry;
+import java.io.FileFilter;
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.Repository;
+import javax.swing.ProgressMonitor;
 
 /**
  * <p>Title: </p>
@@ -53,6 +57,7 @@ public class UI extends JFrame {
     private JTextArea jTextArea1 = new JTextArea();
     private Border border4;
     private TitledBorder titledBorder4;
+    private JButton jButtonSearch = new JButton();
 
     public UI() {
         try {
@@ -85,9 +90,14 @@ public class UI extends JFrame {
         jButtonRun.setPreferredSize(new Dimension(73, 27));
         jButtonRun.setText("Run");
         jButtonCancel.setText("Cancel");
+        jButtonCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                jButtonCancel_actionPerformed(e);
+            }
+        });
         jPanel1.setLayout(borderLayout2);
         jPanel3.setLayout(borderLayout3);
-        jTextFieldDirJar.setPreferredSize(new Dimension(300, 21));
+        jTextFieldDirJar.setPreferredSize(new Dimension(230, 21));
         jButtonBrowse.setText("Browse");
         jButtonBrowse.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -96,6 +106,7 @@ public class UI extends JFrame {
         });
         jLabelSelectDir.setText("Select Directory/Jar File");
         jPanelDirJar.setLayout(flowLayout1);
+        flowLayout1.setAlignment(FlowLayout.LEFT);
         flowLayout1.setHgap(10);
         jPanelTests.setLayout(borderLayout4);
         jScrollPaneTestSuites.setBorder(titledBorder2);
@@ -106,12 +117,24 @@ public class UI extends JFrame {
         jSplitPaneTests.setContinuousLayout(true);
         jTextArea1.setPreferredSize(new Dimension(70, 125));
         jScrollPane3.setBorder(titledBorder4);
+        this.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                this_windowClosing(e);
+            }
+        });
+        jButtonSearch.setText("Search");
+        jButtonSearch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                jButtonSearch_actionPerformed(e);
+            }
+        });
         jPanelDirJar.add(jLabelSelectDir, null);
         this.getContentPane().add(jLabel1,  BorderLayout.NORTH);
         this.getContentPane().add(jPanel1, BorderLayout.CENTER);
         jPanel1.add(jPanel3, BorderLayout.NORTH);
         jPanel3.add(jPanelDirJar, BorderLayout.CENTER);
         jPanelDirJar.add(jTextFieldDirJar, null);
+        jPanelDirJar.add(jButtonSearch, null);
         jPanelDirJar.add(jButtonBrowse, null);
         jPanel1.add(jPanelTests, BorderLayout.CENTER);
         this.getContentPane().add(jPanel2, BorderLayout.SOUTH);
@@ -129,6 +152,8 @@ public class UI extends JFrame {
     public static void main(String[] args) {
         UI ui= new UI();
         ui.setSize(600,500);
+        ui.pack();
+        ui.center();
         ui.show();
     }
 
@@ -136,25 +161,24 @@ public class UI extends JFrame {
         //search jar file for classes that are instances of PLutinTestCase or PluginTestSuite
         try {
             JarFile jarFile = new JarFile(file);
+            int count = 0;
             for (Enumeration entries = jarFile.entries(); entries.hasMoreElements(); ) {
                 JarEntry entry = (JarEntry)entries.nextElement();
                 String fileName = entry.getName();
                 if (fileName.endsWith(".class")) {
-                    String className = fileName.substring(0, fileName.lastIndexOf(".class"));
-                    className = className.replace('/', '.');
                     try {
-                        Class clazz = this.getClass().forName(className);
-                        Class superClazz = clazz.getSuperclass();
-                        if (superClazz.equals(PluginTestCase.class)) {
-                            testCaseModel.addElement(clazz);
+                        ClassParser cp = new ClassParser(jarFile.getInputStream(entry), fileName);
+                        JavaClass jc = cp.parse();
+                        if (Repository.instanceOf(jc, "org.cougaar.plugin.test.PluginTestCase")) {
+                            testCaseModel.addElement(jc.getClassName());
                         }
-                        else if (superClazz.equals(PluginTestSuite.class)) {
-                            testSuiteModel.addElement(clazz);
+                        else if (Repository.instanceOf(jc,"org.cougaar.plugin.test.PluginTestSuite")) {
+                            testSuiteModel.addElement(jc.getClassName());
                         }
                     }
-                    catch (RuntimeException rtw){}
-                    catch (Exception e) {}
-
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
                 }
             }
         }
@@ -163,19 +187,72 @@ public class UI extends JFrame {
 
 
     void jButtonBrowse_actionPerformed(ActionEvent e) {
+
         JFileChooser jfc = new JFileChooser();
         jfc.setDialogTitle("Select directory or jar file");
+        jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        jfc.setFileFilter(new javax.swing.filechooser.FileFilter() {
+            public boolean accept(File pathname) {
+                if (pathname.getName().endsWith(".jar") || pathname.isDirectory())
+                    return true;
+                return false;
+            }
+            public String getDescription() {
+                return "Jars and Dirs";
+            }
+        });
         int ret = jfc.showOpenDialog(this);
         if (ret == JFileChooser.APPROVE_OPTION) {
-            File f = jfc.getSelectedFile();
-            if (f.isDirectory()) {
-                //search for class and jar files that are instance of PluginTestCase or PluginTestSuite
-
-            }
-            else if (f.getName().toLowerCase().endsWith(".jar")) {
-                searchJarFile(f);
-            }
+            jTextFieldDirJar.setText(jfc.getSelectedFile().getAbsolutePath());
         }
 
+    }
+
+    void jButtonCancel_actionPerformed(ActionEvent e) {
+        System.exit(1);
+    }
+
+    void this_windowClosing(WindowEvent e) {
+        System.exit(1);
+    }
+
+    private void center () {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension windowSize = this.getSize();
+        this.setLocation((screenSize.width - windowSize.width)/2, (screenSize.height - windowSize.height)/2);
+    }
+
+    void jButtonSearch_actionPerformed(ActionEvent e) {
+        Thread t = new Thread() {
+            public void run() {
+                try {
+                    File f = new File(jTextFieldDirJar.getText());
+                    if (f.isDirectory()) {
+                        //search for class and jar files that are instance of PluginTestCase or PluginTestSuite
+                        File[] jarFiles = f.listFiles( new FileFilter() {
+                                    public boolean accept(File pathname) {
+                                    if (pathname.getName().endsWith(".jar"))
+                                        return true;
+                                    return false;
+                                    }
+                        });
+                        SearchProgressDialog spd = new SearchProgressDialog("Searching for test cases/suites...", "", 0,0);
+                        spd.setProgressValue(0);
+                        int count = 0;
+                        for (int i=0; i<jarFiles.length; i++) {
+                            spd.setNote(jarFiles[i].getName());
+                            spd.setProgressValue(count++);
+                            searchJarFile(jarFiles[i]);
+                        }
+                        spd.close();
+                    }
+                    else if (f.getName().toLowerCase().endsWith(".jar")) {
+                        searchJarFile(f);
+                    }
+                }
+                catch (Exception e2){}
+            }
+        };
+        t.start();
     }
 }
