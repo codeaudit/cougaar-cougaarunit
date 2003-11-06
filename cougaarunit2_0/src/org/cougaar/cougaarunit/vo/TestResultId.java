796f453e80ce6a85d652bb39651a40e281610033
ttschampel <ttschampel>
2003-11-06 21:40:44 +0000

    Added value objects for test results

---
+++ cougaarunit2_0/src/org/cougaar/cougaarunit/vo/TestResultId.java
@@ -0,0 +1,83 @@
+/*
+ * Created on Nov 6, 2003
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.cougaar.cougaarunit.vo;
+
+import java.io.Serializable;
+
+/**
+ * @author ttschampel
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class TestResultId implements Serializable{
+	
+	
+	
+	private String phase;
+	private String command;
+	private String description;
+	private String result;
+
+	
+	/**
+	 * @return Returns the command.
+	 */
+	public String getCommand() {
+		return command;
+	}
+
+	/**
+	 * @param command The command to set.
+	 */
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	/**
+	 * @return Returns the description.
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description The description to set.
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return Returns the phase.
+	 */
+	public String getPhase() {
+		return phase;
+	}
+
+	/**
+	 * @param phase The phase to set.
+	 */
+	public void setPhase(String phase) {
+		this.phase = phase;
+	}
+
+	/**
+	 * @return Returns the result.
+	 */
+	public String getResult() {
+		return result;
+	}
+
+	/**
+	 * @param result The result to set.
+	 */
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+}
